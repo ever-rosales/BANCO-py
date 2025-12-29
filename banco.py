42bcdaf9e1e27c17536b8ae96ba783c4bf46b5f8
Ever <daftsure9@gmail.com>
2025-12-29 13:59:00 -0600

    Esqueleto del banco

---
+++ banco.py
@@ -0,0 +1,19 @@
+"""
+Funciones de un banco
+-Crear usuario
+-Estado de cuenta
+-Retiro de dinero
+-Ingresar dinero
+"""
+def usuario ():
+    datos = {
+        "Nombre": "Ever",
+        "Apellido": "Rosales",
+        "Fecha de nacimiento": 24,
+        "Numero de celular": 5563372685
+        "Usuario": 
+        "Contraseña": 
+    }
+    print(datos)
+
+usuario ()
